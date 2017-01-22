262e78b368a09244cc535ce75250be08cdc81020
Gerben Heinen <gerbenheinen@gmail.com>
2017-01-22 19:49:15 +0100

    add graphic output for ruby to grid

--- binary_tree_demo.rb
+++ binary_tree_demo.rb
@@ -4,3 +4,5 @@ require 'binary_tree'
 grid = Grid.new(4,4)
 BinaryTree.on(grid)
 puts grid
+img = grid.to_png
+img.save "maze_binarytree.png"
--- sidewinder_demo.rb
+++ sidewinder_demo.rb
@@ -4,3 +4,5 @@ require 'sidewinder'
 grid = Grid.new(4,4)
 Sidewinder.on(grid)
 puts grid
+img = grid.to_png cell_size: 50
+img.save "maze_sidewinder.png"
